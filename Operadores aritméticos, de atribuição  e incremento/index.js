c1e0471b99dfb17ce69f060d60da004f84134500
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-07 12:07:58 -0300

    update

--- Operadores aritméticos, de atribuição  e incremento/index.js
+++ Operadores aritméticos, de atribuição  e incremento/index.js
@@ -7,6 +7,13 @@ Aritméticos
 + = Adição / Concatenação
 - = Subtração
 */
+/*
+Incremento = ++
+Decremento = --
+*/
+/*
+Operadores de atribuição
+*/
 const numString = "5";
 const num1 = 5;
 const num2 = 2;
@@ -20,3 +27,30 @@ console.log(num1 ** num2);
 console.log(num1 % num2);
 
 console.log((num1 + num2) * num3);
+console.log((num1 * num2) / num3);
+
+const passo = 2;
+let contador = 1;
+contador++;
+contador--;
+++contador;
+--contador;
+contador += passo; //contador = contador + passo
+contador -= passo;
+contador *= passo;
+contador /= passo;
+contador **= passo;
+console.log(contador);
+/*
+NaN - Not a number
+parseInt (inteiro)
+parseFloat (decimais)
+Number (inteiro ou decimal)
+*/
+const num4 = 10;
+const num5 = parseInt("5");
+const num6 = parseFloat("5.2");
+const num7 = Number("2.5");
+console.log(num4 + num5);
+console.log(num4 * num5);
+console.log(typeof num5);
