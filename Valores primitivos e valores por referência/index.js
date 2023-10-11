20a68471efe1cc01704d29fa1666cbdca358df1f
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-11 17:28:17 -0300

    update

---
+++ Valores primitivos e valores por referência/index.js
@@ -0,0 +1,21 @@
+//Valores primitivos (Imutáveis) = string, number, boolean, undefined, null, bigint e symbol - Valores copiados
+let a = "A";
+let b = a; //Cópia
+console.log(a, b); //A A
+a = "Outra coisa";
+console.log(a, b); //Outra coisa A
+
+//Valores por referência (Mutável) = array, object e function - Valores passados por referência
+let c = [1, 2, 3];
+let d = c;
+console.log(c, d); //[1, 2, 3] [1, 2, 3]
+c.push(4);
+console.log(c, d); //[1, 2, 3, 4] [1, 2, 3, 4]
+
+const e = {
+  nome: "Wesley",
+  sobrenome: "Victor",
+};
+const f = e;
+e.nome = "João";
+console.log(f); //{nome: "João", sobrenome: "Victor"}
