3455e8853684aa2098c14897c4e6bce72fae24b6
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-11 19:18:44 -0300

    update

---
+++ Seção 2 - JavaScript - Lógica de programação/Operadores de comparação/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+>   | Maior que: Compara se o valor à esquerda é maior que o valor à direita.
+>=  | Maior que ou igual a: Compara se o valor à esquerda é maior ou igual ao valor à direita.
+<   | Menor que: Compara se o valor à esquerda é menor que o valor à direita. 
+<=  | Menor que ou igual a: Compara se o valor à esquerda é menor ou igual ao valor à direita.
+==  | Igualdade (valor): Compara se os valores são iguais, independentemente do tipo de dado.
+=== | Igualdade estrita (valor e tipo): Compara se os valores e os tipos de dados são idênticos.
+!=  | Diferente (valor): Compara se os valores são diferentes, independentemente do tipo de dado.
+!== | Diferente estrito (valor e tipo): Compara se os valores e os tipos de dados são diferentes.
+*/
+console.log(10 > 5); //Verifica se 10 é maior do que 5 e retorna verdadeiro (true).
+console.log(10 >= 11); //Verifica se 10 é maior ou igual a 11 e retorna falso (false).
+console.log(10 < 11); //Verifica se 10 é menor do que 11 e retorna verdadeiro (true).
+console.log(12 <= 11); //Verifica se 12 é menor ou igual a 11 e retorna falso (false).
+console.log(10 == "10"); //Verifica se o number 10 é igual a string 10 e retorna verdadeiro (true).
+console.log(10 === "10"); //Verifica se o number 10 é igual a string 10 e retorna falso (false).
+console.log(10 != "10"); //Verifica se o number 10 é diferente da string 10 e retorna falso (false).
+console.log(10 !== "10"); //Verifica se o number 10 é diferente da string 10 e retorna verdadeiro (true).
