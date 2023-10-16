aee9470dbc74c61eb0a6822d6d8f9a8b5a156032
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-16 15:43:07 -0300

    update

---
+++ Seção 2 - JavaScript - Lógica de programação/For - Clássico - Estrutura de repetição/index.js
@@ -0,0 +1,5 @@
+//Um loop "for" em JavaScript é uma estrutura que permite que você execute um bloco de código repetidamente
+//enquanto uma condição específica for verdadeira. Ele consiste em três partes principais: inicialização, 
+//condição e atualização da variável de controle. O loop executa o código dentro do bloco até que a condição se 
+//torne falsa. Isso é útil para automatizar tarefas repetitivas.
+
