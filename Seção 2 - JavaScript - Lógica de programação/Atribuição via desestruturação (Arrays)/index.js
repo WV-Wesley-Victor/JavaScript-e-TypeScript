1f696e280b1fac79cbcea837730a529f182bc691
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-16 15:10:49 -0300

    update

--- Seção 2 - JavaScript - Lógica de programação/Atribuição via desestruturação (Arrays)/index.js
+++ Seção 2 - JavaScript - Lógica de programação/Atribuição via desestruturação (Arrays)/index.js
@@ -6,3 +6,16 @@ let c = "C"; //Declara e inicializa uma variável 'c' com o valor "C".
 const letras = [b, c, a]; //Cria um array 'letras' com os valores de 'b', 'c' e 'a' naquela ordem.
 [a, b, c] = letras; //Desestrutura o array 'letras' para as variáveis 'a', 'b' e 'c', reatribuindo seus valores.
 console.log(a, b, c); //Imprime os valores atualizados de 'a', 'b' e 'c', que agora são "B", "C" e "A".
+
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [um, dois, tres, , , seis, ...resto] = numeros; //Desestruturação do array 'numeros' em variáveis.
+console.log(um, dois, tres, seis); //Imprime os valores das variáveis desestruturadas.
+console.log(resto); //Imprime o array 'resto' que contém os elementos restantes do array 'numeros'.
+
+const numero = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+const [lista1, lista2, lista3] = numero; //Desestruturação das listas internas em variáveis separadas.
+console.log(lista3[2]); //Acessando um elemento específico da terceira lista (lista3).
