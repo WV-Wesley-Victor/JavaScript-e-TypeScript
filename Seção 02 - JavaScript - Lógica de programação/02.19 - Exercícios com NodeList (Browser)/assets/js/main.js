ea0744fec474fd7b0a45ea50da91c39deed574e8
Wesley Victor <vitorwesley1013m@gmail.com>
2024-05-31 17:58:13 -0300

    update

---
+++ Seção 02 - JavaScript - Lógica de programação/02.19 - Exercícios com NodeList (Browser)/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafos = document.querySelector(".paragrafos");
+const ps = paragrafos.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for (let p of ps) {
+    console.log(p);
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = "white";
+}
